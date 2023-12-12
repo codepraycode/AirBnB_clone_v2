8b1ae501c397ff6389bb19ab8ca17443547c71d4
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-12-12 11:56:06 +0100

    Updated scripts

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!usr/bin/env bash
 # Setup a webserver for deployment
-sudo apt install -y
-sudo apt install -y nginx
+apt install -y
+apt install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
@@ -14,7 +14,7 @@ echo "<!DOCTYPE html>
     </head>
 </html>" | tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-sudo service nginx restart
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+service nginx restart
